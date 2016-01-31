de9bd406693d6396ff988f072f7fed55551fbc87
jagzuk <andy@andymel.com>
2016-01-31 18:02:46 +0000

    Added Pitch to repository

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
-library(ggplot2)
+deplylibrary(ggplot2)
 
 shinyUI(pageWithSidebar(
 
@@ -15,8 +15,8 @@ shinyUI(pageWithSidebar(
       p("The chart above shows the mean temprerature recorded for the selected season (Spring, Summer, Autumn, Winter, or All Seasons). Use the controls on the left to choose the season and to add Linear Model and/or Loess Model trend lines and confidence intervals."),
       p("The data for this application was obtained from the", a(href="http://www.metoffice.gov.uk/climate/uk/summaries/datasets", "UK Government Climate Datasets")),
       p("This application was created by R A Jagger (jagzuk) for the Coursera Developing Data Products course, part of the Data Science Specialization by Johns Hopkins University."),
-      p("The source code for this application is availale in the author's", a(href="https://github.com/jagzuk", "GitHub respository")),
-      p("You may also be interested in this", a(href="http://fabrickids.co.uk", "Promotional Slide Deck"))
+      p("The source code for this application is availale in the author's", a(href="https://github.com/jagzuk/ddp-uk-climate-mean-temp", "GitHub respository")),
+      p("You may also be interested in this", a(href="http://rpubs.com/jagzuk/ukclimatexplorer", "Promotional Slide Deck"))
       
       )
 
